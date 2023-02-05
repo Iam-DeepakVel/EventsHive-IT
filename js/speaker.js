c1fd26ff27b33c90f5e298b1d9deecb01a42fe99
Iam-DeepakVel <deepak.vel82@gmail.com>
2023-02-06 00:51:17 +0530

    Feat: Footer & UI updates

--- js/speaker.js
+++ js/speaker.js
@@ -1,38 +1,38 @@
 const speakers = [
   {
-    name: "Masha Niv",
+    name: "John Doe",
     image:
       "https://images.unsplash.com/photo-1513910367299-bce8d8a0ebf6?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
     designation:
-      "Professor,Institute of BioChemistry,Food Science and Nutrition",
+      "Associate Professor, Department of Computer Science",
+  },
+  {
+    name: "Jane Smith",
+    image:
+      "https://images.unsplash.com/photo-1540569014015-19a7be504e3a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8ODF8fHBlcnNvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
+    designation:
+      "Assistant Professor, School of Medicine",
   },
   {
     name: "Masha Niv",
     image:
-      "https://images.unsplash.com/photo-1513910367299-bce8d8a0ebf6?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
+      "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDV8fHBlcnNvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
     designation:
       "Professor,Institute of BioChemistry,Food Science and Nutrition",
   },
   {
-    name: "Masha Niv",
+    name: "Emily Wilson",
     image:
-      "https://images.unsplash.com/photo-1513910367299-bce8d8a0ebf6?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
+      "https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjN8fHBlcnNvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
     designation:
-      "Professor,Institute of BioChemistry,Food Science and Nutrition",
+      "Assistant Professor of Mathematics, School of Natural Sciences",
   },
   {
-    name: "Masha Niv",
+    name: "Sarah Davis",
     image:
-      "https://images.unsplash.com/photo-1513910367299-bce8d8a0ebf6?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
+      "https://images.unsplash.com/photo-1544723795-3fb6469f5b39?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDJ8fHBlcnNvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
     designation:
-      "Professor,Institute of BioChemistry,Food Science and Nutrition",
-  },
-  {
-    name: "Masha Niv",
-    image:
-      "https://images.unsplash.com/photo-1513910367299-bce8d8a0ebf6?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80",
-    designation:
-      "Professor,Institute of BioChemistry,Food Science and Nutrition",
+      "Associate Dean, Faculty of Arts and Sciences",
   },
 ];
 
